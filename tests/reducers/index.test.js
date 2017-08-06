62e01bb3e7c813d57ab0816d98c03620e2b0792f
A. Ishikawa <a.ishikawa810@gmail.com>
2017-08-06 20:51:03 +0900

    Install jest for testing reducers

---
+++ tests/reducers/index.test.js
@@ -0,0 +1,20 @@
+import * as Actions from 'actions/index';
+import reducers from 'reducers/index';
+
+describe('index reducers', () => {
+  it('increment', () => {
+    const state = {
+      counter: 0
+    };
+    const actual = reducers.index(state, Actions.increment());
+    expect(actual.counter).toBe(1);
+  });
+
+  it('decrement', () => {
+    const state = {
+      counter: 0
+    };
+    const actual = reducers.index(state, Actions.decrement());
+    expect(actual.counter).toBe(-1);
+  });
+});
