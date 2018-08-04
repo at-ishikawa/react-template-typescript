aebc63d93bfec051f352833a3b0a4eb9e2e27218
A. Ishikawa <a.ishikawa810@gmail.com>
2018-08-04 22:12:55 +0900

    Remove google tag manager component

--- src/js/components/layouts/DefaultContainer.js
+++ src/js/components/layouts/DefaultContainer.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import Header from 'components/elements/DefaultHeader';
 import Footer from 'components/elements/DefaultFooter';
-import GoogleTagManager from '../GoogleTagManager';
-import Env from 'Env';
 
 import style from 'layouts/container.css';
 
@@ -11,7 +9,6 @@ class Container extends React.Component {
   render() {
     return (
       <div className={ style.container }>
-        <GoogleTagManager gtmId={Env.gtmId} />
         <Header />
         <main className={ style.main }>
           {this.props.children}
